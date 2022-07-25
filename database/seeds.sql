e8841fd894fe173b0c955a9a12bba8aa11cd756d
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-07-25 16:14:08 -0300

    create and seed tables

---
+++ database/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO categories (category_name)
+VALUES
+('Hardware');
+
+INSERT INTO users (user_name, user_email)
+VALUES
+('Karolina Redden', 'dubajkaro@gmail.com');
+
+
+INSERT INTO tools (tool_name, tool_category_id, tool_owner_id)
+VALUES
+('Hammer', 1, 1);
--- database/tables.sql
+++ database/tables.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS tools CASCADE;
 DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE categories (
 category_id SERIAL PRIMARY KEY NOT NULL,
@@ -9,12 +10,12 @@ category_name VARCHAR(255) NOT NULL
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY NOT NULL,
   user_name VARCHAR(255) NOT NULL,
-  user_email VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255),
   user_password_hash TEXT,
-  user_postal_code VARCHAR(255) NOT NULL,
-  user_address VARCHAR(255) NOT NULL,
-  user_city VARCHAR(255) NOT NULL,
-  user_picture VARCHAR(255) NOT NULL
+  user_postal_code VARCHAR(255),
+  user_address VARCHAR(255),
+  user_city VARCHAR(255),
+  user_picture VARCHAR(255)
 );
 
 CREATE TABLE tools (
@@ -22,7 +23,7 @@ CREATE TABLE tools (
   tool_name VARCHAR(255) NOT NULL,
   tool_category_id INTEGER REFERENCES categories(category_id) ON DELETE CASCADE,
   tool_owner_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
-  tool_picture VARCHAR(255) NOT NULL,
+  tool_picture VARCHAR(255),
   tool_available BOOLEAN
 );
 

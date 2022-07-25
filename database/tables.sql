8e9019ed9015160e23004ab5ef9a2ca7af630a47
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-07-25 15:55:30 -0300

    add tables

---
+++ database/tables.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS tools CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+
+CREATE TABLE categories (
+category_id SERIAL PRIMARY KEY NOT NULL,
+category_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY NOT NULL,
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL,
+  user_password_hash TEXT,
+  user_postal_code VARCHAR(255) NOT NULL,
+  user_address VARCHAR(255) NOT NULL,
+  user_city VARCHAR(255) NOT NULL,
+  user_picture VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tools (
+  tool_id SERIAL PRIMARY KEY NOT NULL,
+  tool_name VARCHAR(255) NOT NULL,
+  tool_category_id INTEGER REFERENCES categories(category_id) ON DELETE CASCADE,
+  tool_owner_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+  tool_picture VARCHAR(255) NOT NULL,
+  tool_available BOOLEAN
+);
+
+

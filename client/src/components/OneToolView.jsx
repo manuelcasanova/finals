2cd07a8342a456bab688208647fd1f8f0bde4624
Manuel Casanova <manucasanova@hotmail.com>
2022-07-26 13:03:07 -0700

    Add classNames and styling

--- client/src/components/OneToolView.jsx
+++ client/src/components/OneToolView.jsx
@@ -1,5 +1,14 @@
 export default function OneToolView () {
   return (
-    <div>One tool view</div>
+    <div className="one-item-container">
+          
+    <div className="one-item-picture">Picture</div>
+    <div className="one-item-information-container">
+      <div className="one-item-name">Name</div>
+      <div className="one-item-status">Available</div>
+      <div className="one-item-owner-name">Owner name name name name</div>
+      <div className="one-item-owner-email">Owner email email email</div>
+    </div>
+  </div>
   )
 }

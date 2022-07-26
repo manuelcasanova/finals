8a01a2036a95166e0896324666b8315b22a8d2d8
Liam Smith <liam.s.smith@gmail.com>
2022-07-26 00:47:24 +0000

    add seed data

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,12 +1,31 @@
 INSERT INTO categories (category_name)
 VALUES
-('Hardware');
+('Home Maintenance'), ('Yard and Garden'), ('Carpentry and Woodworking'), ('Active Outdoor'), ('Plumbing'); 
 
 INSERT INTO users (user_name, user_email)
 VALUES
-('Karolina Redden', 'dubajkaro@gmail.com');
+('Karolina Redden', 'dubajkaro@gmail.com'),
+('Kilgore Trout', 'k.trout@vonnegut.com'),
+('Alice N. Wonderland', 'alice@therabbithole.com'),
+('Don Quixote', 'dq@lamancha.com'),
+('Essun Oh', 'essun@orogenes.org'),
+('Sam Bell', 'sam@moonmining.com'),
+('Selina Kyle', 'catwoman@superheroes.org');
+
 
 
 INSERT INTO tools (tool_name, tool_category_id, tool_owner_id)
 VALUES
-('Hammer', 1, 1);
+('Hammer', 1, 1),
+('Dewalt Power Drill', 1, 5),
+('Ratchet Set', 1, 4),
+('Electric Lawn Mower', 2, 2),
+('Hedge Clippers', 2, 7),
+('Compound Mitre Saw', 3, 3),
+('Circular Saw', 3, 3),
+('Jointer', 3, 6),
+('Bicycle Tire Irons', 4, 5),
+('4 Person Tent', 4, 7),
+('Table Top BBQ', 4, 1),
+('Pipe Wrench', 5, 3),
+('Drain Auger', 5, 6)

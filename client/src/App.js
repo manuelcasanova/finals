cf00d834fc2b31708e576420db193adf600d256e
Manuel Casanova <manucasanova@hotmail.com>
2022-07-25 17:50:51 -0700

    Write divs, future components and prepare to apply styling

--- client/src/App.js
+++ client/src/App.js
@@ -12,14 +12,45 @@ function App() {
 
   return (
     <div className="app">
-      Authentication strip
-      Navbar
-      Search
-      Filter
+      <div className="authentication">
+        <div className="authentication-left">
+          <div className="logo">Logo</div>
+        </div>
+        <div className="authentication-right">
+          <div className="login-as-user">Login as user</div>
+          <div className="login-as-admin">Login as admin</div>
+        </div>
+
+      </div>
+      <div className="navbar">
+        <div className="navbar-left">
+          <div className="navbar-button">Inventory</div>
+          <div className="navbar-button">Groups</div>
+          <div className="navbar-button">About us</div>
+        </div>
+        <div className="navbar-right">
+          <div className="navbar-dropdown">My account</div>
+          <div className="navbar-dropdown">Administrator</div>
+        </div>
+      </div>
+
+      <div className="searchbar">
+        <div className="searchbar-text">What are you looking for?</div>
+        <div className="searchbar-cateogries-dropdown">Categories dropdown</div>
+        <div className="searchbar-groups-dropdwon">Groups dropdown</div>
+        <div className="searchbar-search-button">Search button</div>
+      </div>
+
+      <div className="filter">
+        <div className="filter-per-page">Filter per page</div>
+        <div className="filter-view">View</div>
+        <div className="filter-sort-by">Sort by</div>
+      </div>
+
       <ShowTools tools={tools}/>
-      Pagination
-      Footer
-      
+
+      <div className="pagination">Pagination</div>
+      <div className="footer">Footer</div>
     </div>
   );
 }

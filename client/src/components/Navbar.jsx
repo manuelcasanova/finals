21b2d77b231343aca76c6a347acdee2cecdf20d2
Manuel Casanova <manucasanova@hotmail.com>
2022-07-26 17:39:10 -0700

    Move navbar styling to a diffent file. Create navbar My Account and Administrator dropdown

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,31 @@
-export default function Navbar () {
+import './styling/navbar.css'
+
+export default function Navbar() {
   return (
     <div className="navbar">
-    <div className="navbar-left">
-      <div className="navbar-button">Inventory</div>
-      <div className="navbar-button">Groups</div>
-      <div className="navbar-button">About us</div>
+      <div className="navbar-left">
+        <div className="navbar-button">Inventory</div>
+        <div className="navbar-button">Groups</div>
+        <div className="navbar-button">About us</div>
+      </div>
+      <div className="navbar-right">
+        <div className="navbar-dropdown">
+          <a className="dropbtn">My account</a>
+          <div className="navbar-dropdown-content">
+            <a href="#">My items</a>
+            <a href="#">Profile</a>
+          </div>
+        </div>
+
+        <div className="navbar-dropdown">
+          <a className="dropbtn">Administrator</a>
+          <div className="navbar-dropdown-content">
+            <a href="#">Manage categories</a>
+            <a href="#">Manage users</a>
+            <a href="#">Manage tools</a>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="navbar-right">
-      <div className="navbar-dropdown">My account</div>
-      <div className="navbar-dropdown">Administrator</div>
-    </div>
-  </div>
   )
 }

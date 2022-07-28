a43a4041c1de7eb91774c815f8df90b57c504e0a
Manuel Casanova <manucasanova@hotmail.com>
2022-07-27 17:36:14 -0700

    Install chakra. Create LogInButtons component. Import into Authentication

--- client/src/components/Authentication.jsx
+++ client/src/components/Authentication.jsx
@@ -1,5 +1,21 @@
 
 
+// export default function Authentication () {
+//   return (
+//     <div className="authentication">
+//     <div className="authentication-left">
+//      <a href="/"><div className="logo">Tool swap</div></a> 
+//     </div>
+//     <div className="authentication-right">
+//       <div className="login-as-user">Login as user</div>
+//       <div className="login-as-admin">Login as admin</div>
+//     </div>
+//   </div>
+//   )
+// }
+
+import LogInButtons from "./LogInButtons";
+
 export default function Authentication () {
   return (
     <div className="authentication">
@@ -7,10 +23,9 @@ export default function Authentication () {
      <a href="/"><div className="logo">Tool swap</div></a> 
     </div>
     <div className="authentication-right">
-      <div className="login-as-user">Login as user</div>
+      <div className="login-as-user"><LogInButtons /></div>
       <div className="login-as-admin">Login as admin</div>
     </div>
   </div>
   )
 }
-

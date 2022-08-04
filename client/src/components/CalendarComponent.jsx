da1c4f54288c483e00f8aed5e5a26b6153f8ac0e
Manuel Casanova <manucasanova@hotmail.com>
2022-08-04 08:22:46 -0700

    styling

--- client/src/components/CalendarComponent.jsx
+++ client/src/components/CalendarComponent.jsx
@@ -121,9 +121,6 @@ export default function CalendarComponent({ toolIdParam, user, admin }) {
 
 
 
-
-
-
 {/* <!-- Modal --> */}
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">

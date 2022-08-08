e7c5d8873aff93b355bbf9357ea53b31f8aff467
Karolina Redden <dubajkaro@gmail.com>
2022-08-08 17:18:49 +0000

    add paginate

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+
 export default function Pagination () {
+  const [tools, setTools] = useState();
+
+
+
+  useEffect(() => {
+    axios.get(`http://localhost:8001/tools`)
+    .then(res => {
+      console.log(res.data)
+      setTools(res.data)
+    })
+  }, [])
+
+  
+
   return (
     <div class="pagination">
   <a href="#">&laquo;</a>

01b9297cfd239950503d68d3ff481a7d8aedcb7e
Karolina Redden <dubajkaro@gmail.com>
2022-08-09 18:29:47 +0000

    add css to the about us page

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -1,17 +1,21 @@
+import { Fragment } from 'react'
 import './styling/aboutus.css'
 
 export default function AboutUs () {
   return (
-
+<Fragment>
 <div className='aboutus-component'>
-  <h1> About us </h1>
-  <h2>Why let your tools waste in your garage if your neighbour could use them? Why clutter your shed with tools that get used only occasionally if you could borrow them for that one handyman job you need to get done. There is no need to spend money on individual ownership if your needs could be met through sharing the tools we already have.
+  <h1 className="header"> About ToolSwap 🧰</h1>
+  <h2 className="intro">Why let your tools waste in your garage if your neighbour could use them? Why clutter your shed with tools that get used only occasionally if you could borrow them for that one job you need to get done. There is no need to spend money on individual ownership if your needs could be met through sharing the resources we have as a community.
   </h2>
-  <h2>Whether you are an apartment dweller or a two-story residence owner Tool Swap is for you! We offer a web-based inventory tracking system allowing you to view, add, track and book tools from the comfort of your own home. Our goal is to strengthen local communities and limit barriers to access tools. </h2>
-  <h1> Our team </h1>
+  <h2 className="intro">Whether you are an apartment dweller or a two-story residence owner Tool Swap is for you! We offer a web-based inventory tracking system allowing you to view, add, track and book tools from the comfort of your own home. Our goal is to strengthen local neighbourhood groups and limit barriers to tool access.</h2>
+</div>
+
+<div className='team-component'>
+  <h1 className="header"> Our team 🫶 </h1>
     <div className='team'>
-    <div class="w50 object-fit"><a href="#"> <img src='/Users/karolinadubaj/lighthouse/finals/server/images/IMG_0317.jpeg' alt="broken image" width="100%" /> </a></div>
-    <div><p>Nova Scotia-based hacker mom. In the past felt overwhelmed with all the tools she acquired to complete DIYs projects found on Pinterest. Nowadays she is a happy minimalist believing in access over ownership philosophy.</p></div>
+    <div class="team-picture"><a href="#"> <img src="https://github.com/manuelcasanova/finals/blob/kr/about_us/server/images/IMG_0317.jpeg?raw=true" alt="team image" width="100%" /> </a></div>
+    <div className="team-bio"><p>Nova Scotia-based hacker mom. In the past felt overwhelmed with all the tools she acquired to complete DIYs projects found on Pinterest. Currently, she is a happy minimalist believing in access over ownership philosophy.</p></div>
       
     </div>
 
@@ -23,7 +27,7 @@ export default function AboutUs () {
       
     </div>
 </div>
-
+</Fragment>
   )
 
 }

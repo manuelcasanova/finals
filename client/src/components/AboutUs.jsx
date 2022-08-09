dcb80e6a987dafa924659d3f3af922075dfdad53
Karolina Redden <dubajkaro@gmail.com>
2022-08-09 17:21:45 +0000

    about us text

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -4,7 +4,24 @@ export default function AboutUs () {
   return (
 
 <div className='aboutus-component'>
-  ABOUT US COMPONENT
+  <h1> About us </h1>
+  <h2>Why let your tools waste in your garage if your neighbour could use them? Why clutter your shed with tools that get used only occasionally if you could borrow them for that one handyman job you need to get done. There is no need to spend money on individual ownership if your needs could be met through sharing the tools we already have.
+  </h2>
+  <h2>Whether you are an apartment dweller or a two-story residence owner Tool Swap is for you! We offer a web-based inventory tracking system allowing you to view, add, track and book tools from the comfort of your own home. Our goal is to strengthen local communities and limit barriers to access tools. </h2>
+  <h1> Our team </h1>
+    <div className='team'>
+    <div class="w50 object-fit"><a href="#"> <img src='/Users/karolinadubaj/lighthouse/finals/server/images/IMG_0317.jpeg' alt="broken image" width="100%" /> </a></div>
+    <div><p>Nova Scotia-based hacker mom. In the past felt overwhelmed with all the tools she acquired to complete DIYs projects found on Pinterest. Nowadays she is a happy minimalist believing in access over ownership philosophy.</p></div>
+      
+    </div>
+
+    <div className='team'>
+      
+    </div>
+
+    <div className='team'>
+      
+    </div>
 </div>
 
   )
